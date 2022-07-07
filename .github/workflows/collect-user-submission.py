3944d7e2f2b715785556ce626aa550f6bb5e12f3
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-07-07 17:04:03 -0600

    Update collect-user-submission.py

--- .github/workflows/collect-user-submission.py
+++ .github/workflows/collect-user-submission.py
@@ -36,6 +36,6 @@ class IssueInfo:
 if __name__ == '__main__':
 
     issue = IssueInfo(gh_event_path=os.environ['GITHUB_EVENT_PATH']).create_submission()
-    inputs = issue.submission.dict()
-    with open('cookbook-gallery-submission-input.json', 'w') as f:
-        json.dump(inputs, f)
+    input = issue.submission.dict()
+    with open('cookbook-submission-input.txt', 'w') as f:
+        f.write(input['repo'])
